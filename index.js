d24e494f3728eafb965f3d90a2e113932bb4a8e8
Danielle-29 <daninha137@hotmail.com>
2025-01-15 17:54:08 -0300

    Desenvolvimento do projeto proposto para saber o nível do Herói informado

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Olá Mundão")
+var nome = prompt("Digite o nome do herói");
+var xp = Number(prompt("Digite o nível de experiencia do seu herói"));
+
+var nivel = "";
+
+if (xp <= 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) { 
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000){
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000){
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000){
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000){
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000){
+    nivel = "Imortal"
+} else if (xp >= 10001){
+    nivel = "Radiante";
+} else {
+    nivel = "Indefinido";
+}
+
+
+alert(`O Herói de nome ${nome} está no nível de ${nivel}`);
